82c8021906ff3bce1adf7231993e26f7eca25b5b
maru44 <takumaru22244@gmail.com>
2021-04-21 08:04:06 +0900

    fuck go & db

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"os"
 
+	// "encoding/json"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -14,16 +16,18 @@ import (
 )
 
 type Comment struct {
-	Id      string `dynamodbav:"Id,hash`
+	Person  string `dynamodbav:"Person,hash"`
+	Year    string `dynamodbav:"Year,range"`
 	Content string `dynamodbav:"Content"`
 }
 
 func main() {
-	sess := session.Must(session.NewSession())
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
 
 	db := dynamodb.New(sess, &aws.Config{
-		Region: aws.String("ap-northeast-1"),
-		// Output: aws.String("json"),
+		Region:   aws.String("ap-northeast-1"),
 		Endpoint: aws.String(os.Getenv("DYNAMO_ENDPOINT")),
 		Credentials: credentials.NewStaticCredentials(
 			os.Getenv("AWS_ACCESS_KEY_ID"),
@@ -31,21 +35,62 @@ func main() {
 			os.Getenv("AWS_SESSION_TOKEN"),
 		),
 	})
-	// ここまでは動く
 
 	// delete table
 
-	delParams := &dynamodb.DeleteTableInput{
-		TableName: aws.String("Comments"),
+	/*
+		delParams := &dynamodb.DeleteTableInput{
+			TableName: aws.String("Comments"),
+		}
+		resp, err := db.DeleteTable(delParams)
+		if err != nil {
+			log.Printf("%v", err)
+		}
+	*/
+
+	// create table
+
+	tableName := "Contents"
+	createParams := &dynamodb.CreateTableInput{
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("Year"),
+				AttributeType: aws.String("S"),
+			},
+			{
+				AttributeName: aws.String("Person"),
+				AttributeType: aws.String("S"),
+			},
+			{
+				AttributeName: aws.String("Content"),
+				AttributeType: aws.String("S"),
+			},
+		},
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("Person"),
+				KeyType:       aws.String("HASH"),
+			},
+			{
+				AttributeName: aws.String("Year"),
+				KeyType:       aws.String("RANGE"),
+			},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(10),
+			WriteCapacityUnits: aws.Int64(10),
+		},
+		TableName: aws.String(tableName),
 	}
-	resp, err := db.DeleteTable(delParams)
+
+	_, err := db.CreateTable(createParams)
 	if err != nil {
-		log.Printf("%v", err)
+		log.Fatalf("Error: %s", err)
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %v", resp)
+		fmt.Fprintf(w, "Hello, %v", db)
 	})
 
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8080", nil)
 }
